a5893c74cf9a3583ff8934623798649ef74b35d1
karlmoutome <karlmoutome@gmail.com>
2018-05-21 10:32:47 -0400

    Modification des vues devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  validatable : fullname, presence:true, length : { maximum: 50}
+  validates :fullname, presence: true, length: { maximum: 65}
 end
