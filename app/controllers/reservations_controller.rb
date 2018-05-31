2984c4ca13bd8e3212f639ca74d2ae6e25be6620
karlmoutome <karlmoutome@gmail.com>
2018-05-31 06:11:33 -0400

    Ajout page mes voyages

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,24 +2,8 @@ class ReservationsController < ApplicationController
  
 before_action :authenticate_user! 
 
-def preload
-  room = Room.find(params[:room_id])
-  today = Date.today
-  reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
-  
-  render json :reservations
-end
-  
-def preview
-  start_date = Date.parse(params[:start_date])
-  end_date = Date.parse(params[:end_date])
-  
-  output = {
-            conflict: is_conflict(start_date, end_date)
-        }
-        
-        render json: output
-end
+
+
   
 def create
  
@@ -38,10 +22,3 @@ private
      end 
 end
 
-private
-    def is_conflict(start_date, end_date)
-        room = Room.find(params[:room_id])
- 
-        check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
-        check.size > 0? true : false
-    end
